2d09a85b1a5a52b4e2aa4a84be359e491702526a
Prashant P <craftyprash@gmail.com>
2023-01-21 19:48:00 +0530

    lualine, buffers, icons, plenary

---
+++ lua/craftyprash/plugins/ui.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "auto",
+        },
+      })
+    end,
+  },
+
+   -- buffer remove
+   {
+    "echasnovski/mini.bufremove",
+    -- stylua: ignore
+    keys = {
+      { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
+      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+    },
+  },
+
+  -- bufferline
+  {
+    "akinsho/nvim-bufferline.lua",
+    event = "BufAdd",
+    config = {
+      options = {
+        always_show_bufferline = false,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+  },
+}
